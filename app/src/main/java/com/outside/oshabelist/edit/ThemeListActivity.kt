20a81379d312cfd098815d2c4c54af2b8c757849
t-takumi-takumi <takumi18616@gmail.com>
2021-09-09 22:00:44 +0900

    基本の画面遷移構築

---
+++ app/src/main/java/com/outside/oshabelist/edit/ThemeListActivity.kt
@@ -0,0 +1,25 @@
+package com.outside.oshabelist.edit
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.outside.oshabelist.R
+import com.outside.oshabelist.databinding.ActivityThemeListBinding
+
+class ThemeListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThemeListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityThemeListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportFragmentManager.beginTransaction().add(R.id.content, ThemeListFragment.newInstance())
+            .commit()
+    }
+
+    companion object {
+        fun createIntent(context: Context): Intent = Intent(context, ThemeListActivity::class.java)
+    }
+}
