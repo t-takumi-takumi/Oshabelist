baacdb6aecd9540262fcbfc974522f9ef4788b29
t-takumi-takumi <takumi18616@gmail.com>
2021-09-26 18:35:16 +0900

    ネタ編集画面のツールバー作成

--- app/src/main/java/com/outside/oshabelist/edit/NetaEditFragment.kt
+++ app/src/main/java/com/outside/oshabelist/edit/NetaEditFragment.kt
@@ -1,24 +1,33 @@
 package com.outside.oshabelist.edit
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.outside.oshabelist.R
 
 class NetaEditFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_neta_edit, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_neta_edit, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.saveButton) {
+            Toast.makeText(requireContext(), "save", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return false
     }
 
     companion object {
